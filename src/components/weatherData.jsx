f56b6e06888098125191601561ac88fb8029c797
Aidan Duff <aidanpatrickduff@gmail.com>
2020-08-04 20:00:15 +0100

    added pagination to view 12 records(1 year)  per page

--- src/components/weatherData.jsx
+++ src/components/weatherData.jsx
@@ -1,48 +1,72 @@
 import React, { Component } from "react";
 import WeatherDataService from "../services/weatherDataService";
+import Pagination from "./pagination";
+import { paginate } from "../utils/paginate";
 
 class WeatherData extends Component {
   state = {
     weatherDataItems: [],
+    currentPage: 1,
+    pageSize: 12,
   };
 
   componentDidMount() {
     WeatherDataService.retrieveAllData().then((response) => {
-      response.status === 204
-        ? this.setState({ weatherDataItems: [] })
-        : this.setState({ weatherDataItems: response.data });
+      this.setState({ weatherDataItems: response.data });
     });
   }
 
+  handlePageChange = (page) => {
+    this.setState({ currentPage: page });
+  };
+
   render() {
-    console.log(this.state.weatherDataItems);
+    const { currentPage, pageSize } = this.state;
+    console.log(this.state.weatherDataItems.length);
+
+    if (this.state.weatherDataItems.length === 0) return null;
+
+    const weatherDataItems = paginate(
+      this.state.weatherDataItems,
+      currentPage,
+      pageSize
+    );
+
     return (
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Year</th>
-            <th>Month</th>
-            <th>Indicator</th>
-            <th>Rain</th>
-            <th>GDF</th>
-            <th>RD</th>
-            <th>WD</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.state.weatherDataItems.map((weatherDataItem) => (
-            <tr key={weatherDataItem.id}>
-              <td>{weatherDataItem.year}</td>
-              <td>{weatherDataItem.month}</td>
-              <td>{weatherDataItem.indicator}</td>
-              <td>{weatherDataItem.rain}</td>
-              <td>{weatherDataItem.gdf}</td>
-              <td>{weatherDataItem.rd}</td>
-              <td>{weatherDataItem.wd}</td>
+      <React.Fragment>
+        <table className="table mt-4">
+          <thead>
+            <tr>
+              <th>Year</th>
+              <th>Month</th>
+              <th>Indicator</th>
+              <th>Rainy Days</th>
+              <th>Greatest Daily Rainfall</th>
+              <th>Rainy Days</th>
+              <th>Wet Days</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {weatherDataItems.map((weatherDataItem) => (
+              <tr key={weatherDataItem.id}>
+                <td>{weatherDataItem.year}</td>
+                <td>{weatherDataItem.month}</td>
+                <td>{weatherDataItem.indicator}</td>
+                <td>{weatherDataItem.rain}</td>
+                <td>{weatherDataItem.gdf}</td>
+                <td>{weatherDataItem.rd}</td>
+                <td>{weatherDataItem.wd}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <Pagination
+          weatherDataItemsCount={this.state.weatherDataItems.length}
+          currentPage={currentPage}
+          pageSize={pageSize}
+          onPageChange={this.handlePageChange}
+        />
+      </React.Fragment>
     );
   }
 }
