bf5c85a46e04c5b4ad1aaa97dbd30829f2885704
Aidan Duff <aidanpatrickduff@gmail.com>
2020-08-12 20:01:52 +0100

    Added some security and validation against wrong file formats. Added flash messages

--- src/components/fileUpload.jsx
+++ src/components/fileUpload.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import FlashMessage from "react-flash-message";
 import { post } from "axios";
 
 class FileUpload extends Component {
@@ -6,6 +7,8 @@ class FileUpload extends Component {
     super(props);
     this.state = {
       file: null,
+      buttonStatus: false,
+      showMessage: false,
     };
   }
 
@@ -16,8 +19,22 @@ class FileUpload extends Component {
       console.log(response.data);
     });
   };
+
+  getButtonStatus = () => {
+    return this.state.buttonStatus
+      ? "btn btn-primary"
+      : "btn btn-primary disabled";
+  };
+
   onChange = (e) => {
-    this.setState({ file: e.target.files[0] });
+    const str = e.target.files[0].name;
+    if (str.endsWith(".csv")) {
+      this.setState({ file: e.target.files[0], buttonStatus: true });
+    } else {
+      this.setState({ showMessage: true });
+    }
+
+    console.log(e.target.files[0]);
   };
   fileUpload = (file) => {
     const url = "http://localhost:8080/upload";
@@ -33,26 +50,33 @@ class FileUpload extends Component {
 
   render() {
     return (
-      <form onSubmit={this.onFormSubmit} className="m-4">
-        <h1>File Upload</h1>
-        <div className="custom-file">
-          <div className="input-group">
-            <input
-              type="file"
-              accept="text/csv, .csv"
-              className="mb-2"
-              onChange={this.onChange}
-            />
+      <React.Fragment>
+        {this.state.showMessage && (
+          <div>
+            <FlashMessage duration={3000}>
+              <div className="alert alert-danger">
+                <p>Selected file must be in .csv format!</p>
+              </div>
+            </FlashMessage>
           </div>
-          <button
-            className="btn btn-primary"
-            type="submit"
-            // onClick={this.props.history.push("/home")}
-          >
-            Upload
-          </button>
-        </div>
-      </form>
+        )}
+        <form onSubmit={this.onFormSubmit} className="m-4">
+          <h1>File Upload</h1>
+          <div className="custom-file">
+            <div className="input-group">
+              <input
+                type="file"
+                accept="text/csv, .csv"
+                className="mb-2"
+                onChange={this.onChange}
+              />
+            </div>
+            <button className={this.getButtonStatus()} type="submit">
+              Upload
+            </button>
+          </div>
+        </form>
+      </React.Fragment>
     );
   }
 }
