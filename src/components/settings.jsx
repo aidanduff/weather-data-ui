42a7ad83a0be0cf618721f1335d855e37043229d
Aidan Duff <aidanpatrickduff@gmail.com>
2020-08-16 15:12:38 +0100

    added flash messaging for delete component

--- src/components/settings.jsx
+++ src/components/settings.jsx
@@ -1,23 +1,48 @@
 import React from "react";
 import WeatherDataService from "../services/weatherDataService";
+import Message from "./message";
+import { Component } from "react";
 
-const Settings = () => {
-  const handleClick = () => {
-    return WeatherDataService.deleteAllData();
+class Settings extends Component {
+  state = {
+    showMessage: false,
+  };
+  handleClick = () => {
+    WeatherDataService.deleteAllData();
+    this.setState(
+      {
+        showMessage: true,
+      },
+      () =>
+        setTimeout(() => {
+          this.setState({ showMessage: false });
+        }, 3000)
+    );
   };
 
-  return (
-    <React.Fragment>
-      <h1 className="pt-4">Delete</h1>
-      <button
-        className="btn btn-primary btn-block"
-        onClick={handleClick}
-        type="submit"
-      >
-        Delete
-      </button>
-    </React.Fragment>
-  );
-};
+  render() {
+    const { showMessage } = this.state;
+    return (
+      <div className="contentContainer">
+        {showMessage && (
+          <Message
+            showMessage={showMessage}
+            messageClass={"alert alert-success"}
+            duration={3000}
+            message={"All files deleted"}
+          />
+        )}
+        <h1 className="mt-2">Delete</h1>
+        <button
+          className="btn btn-primary btn-block mt-4"
+          onClick={this.handleClick}
+          type="submit"
+        >
+          Delete
+        </button>
+      </div>
+    );
+  }
+}
 
 export default Settings;
