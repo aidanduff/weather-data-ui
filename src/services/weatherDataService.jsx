f6659fe2d2ee82ae391921dfc8b4c3998cb8a231
Aidan Duff <aidanpatrickduff@gmail.com>
2020-09-01 18:37:08 +0100

    changed api urls for heroku

--- src/services/weatherDataService.jsx
+++ src/services/weatherDataService.jsx
@@ -2,11 +2,13 @@ import axios from "axios";
 
 class WeatherDataService {
   retrieveAllData() {
-    return axios.get(`http://localhost:8080/weather`);
+    // return axios.get(`http://localhost:8080/weather`);
+    return axios.get("https://rainydays-server.herokuapp.com/weather");
   }
 
   addRecord(file) {
-    const url = "http://localhost:8080/upload";
+    // const url = "http://localhost:8080/upload";
+    const url = "https://rainydays-server.herokuapp.com/upload";
     const formData = new FormData();
     formData.append("file", file);
     const config = {
@@ -18,7 +20,8 @@ class WeatherDataService {
   }
 
   deleteAllData() {
-    axios.delete(`http://localhost:8080/delete`);
+    // axios.delete(`http://localhost:8080/delete`);
+    axios.delete(`https://rainydays-server.herokuapp.com/delete`);
   }
 }
 
