20c2c07204ab38e69432295a3e992484677d585f
Aidan Duff <aidanpatrickduff@gmail.com>
2020-09-02 11:15:44 +0100

    converted weatherDataTable to stateless functional component

--- src/components/weatherDatatable.jsx
+++ src/components/weatherDatatable.jsx
@@ -1,35 +1,32 @@
 import React, { Component } from "react";
 
-class WeatherDataTable extends Component {
-  render(props) {
-    const { weatherDataItems } = this.props;
-    return (
-      <React.Fragment>
-        <table className="table table-bordered">
-          <thead className="thead-dark">
-            <tr>
-              <th>Year</th>
-              <th>Month</th>
-              <th>Precipitation (mm)</th>
-              <th>Greatest Daily Rainfall (mm)</th>
-              <th>Rainy Days</th>
+const WeatherDataTable = ({ weatherDataItems }) => {
+  return (
+    <React.Fragment>
+      <table className="table table-bordered">
+        <thead className="thead-dark">
+          <tr>
+            <th>Year</th>
+            <th>Month</th>
+            <th>Precipitation (mm)</th>
+            <th>Greatest Daily Rainfall (mm)</th>
+            <th>Rainy Days</th>
+          </tr>
+        </thead>
+        <tbody>
+          {weatherDataItems.map((weatherDataItem) => (
+            <tr key={weatherDataItem.id}>
+              <td>{weatherDataItem.year}</td>
+              <td>{weatherDataItem.month}</td>
+              <td>{weatherDataItem.rain}</td>
+              <td>{weatherDataItem.gdf}</td>
+              <td>{weatherDataItem.rd}</td>
             </tr>
-          </thead>
-          <tbody>
-            {weatherDataItems.map((weatherDataItem) => (
-              <tr key={weatherDataItem.id}>
-                <td>{weatherDataItem.year}</td>
-                <td>{weatherDataItem.month}</td>
-                <td>{weatherDataItem.rain}</td>
-                <td>{weatherDataItem.gdf}</td>
-                <td>{weatherDataItem.rd}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </React.Fragment>
-    );
-  }
-}
+          ))}
+        </tbody>
+      </table>
+    </React.Fragment>
+  );
+};
 
 export default WeatherDataTable;
